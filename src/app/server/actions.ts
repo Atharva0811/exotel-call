b5646360276d928d1003b1dc59adeb3b334ca595
Atharva0811 <atharvaus008@gmail.com>
2025-06-25 12:30:29 +0530

    Logging added

--- src/app/server/actions.ts
+++ src/app/server/actions.ts
@@ -1,7 +1,27 @@
 "use server";
+import fs from "fs";
+import path from "path";
+async function writeLog(message: string) {
+  const logFileName = "app.log";
+  const logFilePath = path.join(process.cwd(), "logs", logFileName);
+  console.log(logFilePath);
+  const logDir = path.dirname(logFilePath);
+  if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
+  }
+
+  const timestamp = new Date().toISOString();
+  const logEntry = `${timestamp} : ${message}\n`;
+
+  try {
+    await fs.promises.appendFile(logFilePath, logEntry);
+  } catch (error) {
+    console.error("Failed to write log:", error);
+  }
+}
 
 export async function makeCall(phoneNumberToCall: any) {
-  console.log("Calling:",phoneNumberToCall, new Date().toISOString());
+  console.log("Calling:", phoneNumberToCall, new Date().toISOString());
   const apiKey = process.env.EXOTEL_API_KEY;
   const apiToken = process.env.EXOTEL_API_TOKEN;
   const subdomain = process.env.EXOTEL_SUBDOMAIN;
@@ -28,8 +48,6 @@ export async function makeCall(phoneNumberToCall: any) {
     Url: exomlUrl,
   }).toString();
   const exotelApiUrl = `https://${subdomain}/v1/Accounts/${sid}/Calls/connect`;
-  const SID = "your_exotel_sid";
-  const token = "your_exotel_token";
   const auth = btoa(`${apiKey}:${apiToken}`);
   const requestOptions = {
     method: "POST",
@@ -46,6 +64,7 @@ export async function makeCall(phoneNumberToCall: any) {
 
     if (response.ok) {
       console.log("Exotel API Success:", responseBody);
+      writeLog(responseBody);
       return {
         success: true,
         message: "Call initiated successfully!",
@@ -58,6 +77,7 @@ export async function makeCall(phoneNumberToCall: any) {
         "Body:",
         responseBody
       );
+      writeLog(responseBody);
       return {
         success: false,
         error: `Exotel API call failed: ${response.status} - ${responseBody}`,
